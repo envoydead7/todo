7d3ca44245cc72769bda8377ba3eb89c181a1a64
envoydead7 <mustafaabbas0912@gmail.com>
2024-12-16 19:09:49 +0530

    Create a connection to the mongoDB database and create a schema

---
+++ db/connectDB.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+import ora from 'ora';
+import chalk from 'chalk';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export async function connectDB() {
+    try{
+        const spinner = ora('Connecting to the database...').start()
+        await mongoose.connect(process.env.MONGO_URI)
+        spinner.stop();
+        console.log(chalk.greenBright('Successfully Connected to the database...'));
+    }   catch(err){
+        console.log(chalk.redBright(err.message));
+        process.exit(1);
+    }
+}
+
+export async function disconnectDB() {
+    try {
+        await mongoose.disconnect();
+        console.log(chalk.greenBright('Successfully disConnected to the database...'));
+    }
+    catch(err){
+        console.log(chalk.redBright(err.message));
+        process.exit(1);
+    }
+}
+
+connectDB();
+disconnectDB();
